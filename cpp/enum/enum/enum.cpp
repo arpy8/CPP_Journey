3b56538fe182b7be258e36e0816cee3befd5fd2b
Arpit <arpitsengar99@gmail.com>
2023-05-09 00:08:08 +0530

    added program files

---
+++ cpp/enum/enum/enum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+enum day {mon, tue, wed, thu, fri, sat, sun};
+
+int main()
+{
+    cout << mon;
+    cout << tue;
+    cout << wed;
+    cout << thu;
+    cout << fri;
+    cout << sat;
+    cout << sun;
+
+}
---
+++ cpp/typedef/typedef/typedef.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+typedef int marks;
+
+int main()
+{
+    marks m1, m2;
+    m1 = 50;
+    m2 = 120;
+
+}
