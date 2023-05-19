fe01dee113a47749e8cba699e6fbacddb49e3d53
Arpit <arpitsengar99@gmail.com>
2023-05-19 22:58:30 +0530

    added program files

---
+++ cpp/string-operators/string-operators/string-operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+	char str1[] = "Hello World";
+	char str2[] = "Hello CPP";
+
+	// print the length of the song string
+	cout << "strlen(str1) : "  << strlen(str1) << endl;
+
+
+	return 0;
+}
+
+// Output: 17
